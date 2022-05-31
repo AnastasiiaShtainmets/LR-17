b4af16535a735e37f94c3fc52129a4fa0cb5ed27
Anastasiia <nastya589sht@gmail.com>
2022-05-31 21:05:06 +0300

    LR 17

---
+++ src/com/company/Main.java
@@ -0,0 +1,70 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+abstract class Logistic {
+    String Transport;
+    List<String> accessories=new ArrayList();
+    public String toString(){
+        return "Type of transportation:" + "Transport \n"+ accessories;
+
+    }
+}
+abstract class LogisticFactory{
+    public abstract Logistic createLogistic();
+}
+class RoadLogisticFactory extends LogisticFactory {
+    public Logistic createLogistic(){
+        return new RoadLogistic();
+    }}
+class AirLogisticFactory extends LogisticFactory{
+    public Logistic createLogistic(){
+        return new AirLogistic();
+    }}
+class SeaLogisticFactory extends LogisticFactory{
+    public Logistic createLogistic(){
+        return new SeaLogistic();
+    }}
+
+class RoadLogistic extends Logistic{
+    public RoadLogistic(){
+        Transport="RoadLogistic";
+        accessories.add("Capacity in tons");
+        accessories.add("Fuel consumption");
+        accessories.add("Travel time");
+        accessories.add("Navigation system");
+    }
+}
+class AirLogistic extends Logistic{
+    public AirLogistic(){
+        Transport="AirLogistic";
+        accessories.add("Capacity in tons");
+        accessories.add("Fuel consumption");
+        accessories.add("Travel time");
+        accessories.add("Navigation system");
+    }
+}
+class SeaLogistic extends Logistic{
+    public SeaLogistic(){
+        Transport="SeaLogistic";
+        accessories.add("Capacity in tons");
+        accessories.add("Fuel consumption");
+        accessories.add("Travel time");
+        accessories.add("Navigation system");
+    }
+}
+
+public class Main{
+    public static void main(String args[]){
+        LogisticFactory myRoadFactory=new RoadLogisticFactory();
+        LogisticFactory myAirFactory=new AirLogisticFactory();
+        LogisticFactory mySeaFactory=new SeaLogisticFactory();
+        Logistic myRoadLogistic=myRoadFactory.createLogistic();
+        Logistic myAirLogistic=myAirFactory.createLogistic();
+        Logistic mySeaLogistic=mySeaFactory.createLogistic();
+        System.out.println(myRoadLogistic);
+        System.out.println(myAirLogistic);
+        System.out.println(mySeaLogistic);
+    }
+}
